f7436409064007b9d8c1d57fb222e0115fedca46
DumitruLNT <dimamaster333@gmail.com>
2024-02-15 16:51:50 +0200

    no description

--- src/main/java/com/example/dima/app1/app1/service/OrderService.java
+++ src/main/java/com/example/dima/app1/app1/service/OrderService.java
@@ -1,15 +1,16 @@
 package com.example.dima.app1.app1.service;
 
-import com.example.dima.app1.app1.dto.OrderDto;
 import com.example.dima.app1.app1.model.Order;
 
 import java.util.List;
 
 public interface OrderService {
-    List<OrderDto> findAllOrders();
+    List<Order> findAllOrders();
     Order saveOrder(Order order);
 
-    OrderDto findById(long orderId);
+    Order findOrderById(long orderId);
 
-    void updateOrder(OrderDto order);
+    Order updateOrder(Order order);
+
+    Order deleteOrderById(Long id);
 }
